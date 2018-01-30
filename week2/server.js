f90694c7d049cb8fce36ccfe83e68b19ae3e4e11
Dongphil Yoo <dongphilyoo@gmail.com>
2018-01-30 14:23:22 -0500

    initial changes

--- week2/server.js
+++ week2/server.js
@@ -1,18 +1,47 @@
-var express = require('express')
-var app = express()
+//var express = require('express')
+//var app = express()
+//
+//app.get('/', function (req, res) {
+//  res.send('Hello World!')
+//})
+//
+//app.listen(3000, function () {
+//  console.log('Example app listening on port 3000!')
+//})
+////http://IP:3000/
+////Of course a route can be for any "path", here is an example for "/somethingelse":
+//
+//app.get('/somethingelse', function (req, res) {
+//  res.send('Good Bye!');
+//});
+//
+//// git remote sync test
+//
+
+
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+var count = 0;
+
+app.get('/count', function(req, res) {
+  count++;
+  res.send("<html><body><h1>"+count+"</h1></body></html>");
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+});
+
+app.get('/somethingelse', function(req, res) {
+  res.send("Goodbye");
+
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
-//http://IP:3000/
-//Of course a route can be for any "path", here is an example for "/somethingelse":
-
-app.get('/somethingelse', function (req, res) {
-  res.send('Good Bye!');
 });
 
-// git remote sync test
