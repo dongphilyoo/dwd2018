8a5639be82170d3fe97c238510d2e59c1fa17fcf
Dongphil Yoo <dongphilyoo@gmail.com>
2018-02-06 09:56:31 -0500

    add

--- week3/server.js
+++ week3/server.js
@@ -3,36 +3,12 @@ var app = express();
 
 app.use(express.static('public'));
 
-var count = 0;
-
-var thesubmissions = [];
-
-app.set('view engine', 'ejs');
-
-
-app.get('/templatetest', function (req, res) {
-    //	var data = {person: {name: "Shawn", other: "blah"}};
-    var data = [];
-
-    for (var i = 0; i < thesubmissions.length; i++) {
-        data.push(thesubmissions[i]);
-    }
-    res.render('template.ejs', data);
-    //console.log(thesubmissions);
-});
-
 app.get('/formpost', function (req, res) {
-    //res.send("You submitted " + req.query.textfield);
     thesubmissions.push(req.query.textfield);
-    //console.log(thesubmissions);
-    res.redirect('/display');
-
-
-
-    //  res.redirect('/test.html');
+    res.redirect('/undecided');
 });
 
-app.get('/display', function (req, res) {
+app.get('/undecided', function (req, res) {
     var htmlout = "<html><body style='background-color:#FFFF00;'>";
     for (var i = 0; i < thesubmissions.length; i++) {
         var ref = thesubmissions[i].replace(/\s/g, '').toLowerCase();
@@ -41,20 +17,6 @@ app.get('/display', function (req, res) {
     var htmlout = htmlout + "</body></html>";
     res.send(htmlout);
 });
-
-app.get('/count', function (req, res) {
-    count++;
-    res.send("<html><body><h1>" + count + "</h1></body></html>");
-});
-
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-});
-
-app.get('/somethingelse', function (req, res) {
-    res.send("Goodbye");
-
-})
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
