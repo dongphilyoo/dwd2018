8b058a375f30297f854d0231d65c98efcbf85a3e
Dongphil Yoo <dongphilyoo@gmail.com>
2018-02-05 23:40:47 -0500

    initial commit

--- week2/server.js
+++ week2/server.js
@@ -1,31 +1,26 @@
-//var express = require('express')
-//var app = express()
-//
-//app.get('/', function (req, res) {
-//  res.send('Hello World!')
-//})
-//
-//app.listen(3000, function () {
-//  console.log('Example app listening on port 3000!')
-//})
-////http://IP:3000/
-////Of course a route can be for any "path", here is an example for "/somethingelse":
-//
-//app.get('/somethingelse', function (req, res) {
-//  res.send('Good Bye!');
-//});
-//
-//// git remote sync test
-//
-
-
-
 var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
 
 var count = 0;
+
+var thesubmissions = [];
+
+app.get('/formpost', function(req, res) {
+  //res.send("You submitted " + req.query.textfield);
+  thesubmissions.push(req.query.textfield);
+  res.redirect('/display');
+});
+
+app.get('/display', function(req, res) {
+  var htmlout = "<html><body>";
+  for (var i = 0; i < thesubmissions.length; i++) {
+    htmlout = htmlout + thesubmissions[i] + "<br>";
+  }
+  var htmlout = htmlout + "</body></html>";
+  res.send(htmlout);
+});
 
 app.get('/count', function(req, res) {
   count++;
@@ -44,4 +39,3 @@ app.get('/somethingelse', function(req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
-
