768cea4e4bd77404c94ad9fb673f4545cfcd0385
Dongphil Yoo <dongphilyoo@gmail.com>
2018-02-06 10:18:52 -0500

    add

--- week3/server.js
+++ week3/server.js
@@ -20,6 +20,6 @@ app.get('/undecided', function (req, res) {
     res.send(htmlout);
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(5555, function () {
+    console.log('5555')
 });
