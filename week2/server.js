b41f04be9bc17fc0b9410d949153d191eb603711
Dongphil Yoo <dongphilyoo@gmail.com>
2018-02-06 00:09:20 -0500

    server.js update

--- week2/server.js
+++ week2/server.js
@@ -7,36 +7,43 @@ var count = 0;
 
 var thesubmissions = [];
 
-app.get('/formpost', function(req, res) {
-  //res.send("You submitted " + req.query.textfield);
-  thesubmissions.push(req.query.textfield);
-//  res.redirect('/display');
-    
+app.get('/formpost', function (req, res) {
+    //res.send("You submitted " + req.query.textfield);
+    thesubmissions.push(req.query.textfield);
+    //  res.redirect('/display');
+
+    //var htmlout = "<html><body>";
+    for (var i = 0; i < thesubmissions.length; i++) {
+        htmlout = htmlout + thesubmissions[i] + "<br>";
+    }
+    //var htmlout = htmlout + "</body></html>";
+    //res.send(htmlout);
+    window.open("http://www.google.com");
 });
 
-app.get('/display', function(req, res) {
-  var htmlout = "<html><body>";
-  for (var i = 0; i < thesubmissions.length; i++) {
-    htmlout = htmlout + thesubmissions[i] + "<br>";
-  }
-  var htmlout = htmlout + "</body></html>";
-  res.send(htmlout);
-});
-
-app.get('/count', function(req, res) {
-  count++;
-  res.send("<html><body><h1>"+count+"</h1></body></html>");
-});
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-});
-
-app.get('/somethingelse', function(req, res) {
-  res.send("Goodbye");
-
-})
+//app.get('/display', function (req, res) {
+//    var htmlout = "<html><body>";
+//    for (var i = 0; i < thesubmissions.length; i++) {
+//        htmlout = htmlout + thesubmissions[i] + "<br>";
+//    }
+//    var htmlout = htmlout + "</body></html>";
+//    res.send(htmlout);
+//});
+//
+//app.get('/count', function (req, res) {
+//    count++;
+//    res.send("<html><body><h1>" + count + "</h1></body></html>");
+//});
+//
+//app.get('/', function (req, res) {
+//    res.send('Hello World!')
+//});
+//
+//app.get('/somethingelse', function (req, res) {
+//    res.send("Goodbye");
+//
+//})
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!')
 });
