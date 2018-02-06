16a6df6428298008807e82c0599f76e830d37f76
Dongphil Yoo <dongphilyoo@gmail.com>
2018-02-06 09:50:15 -0500

    add

--- week3/server.js
+++ week3/server.js
@@ -33,10 +33,10 @@ app.get('/formpost', function (req, res) {
 });
 
 app.get('/display', function (req, res) {
-    var htmlout = "<html><body style='margin=0;background-color:#FFFF00;'>";
+    var htmlout = "<html><body style='background-color:#FFFF00;'>";
     for (var i = 0; i < thesubmissions.length; i++) {
         var ref = thesubmissions[i].replace(/\s/g, '').toLowerCase();
-        htmlout = htmlout + "<a class='links' href='http://www." + ref + ".com' style='color:#333;font-weight:bold;font-size:3.2rem;margin:2%;display:inline-block;'>" + thesubmissions[i] + "<br>";
+        htmlout = htmlout + "<a class='links' href='http://www." + ref + ".com' style='color:#333;font-weight:bold;font-size:3.2rem;margin:2%;display:inline-block;' onMouseOver= 'this.style.color='#0F0'>' + thesubmissions[i] + "<br>";
     }
     var htmlout = htmlout + "</body></html>";
     res.send(htmlout);
