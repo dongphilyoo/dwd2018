a0a3d2368d6b7758c7de503ed3e12a05b4af1775
Dongphil Yoo <dongphilyoo@gmail.com>
2018-02-12 19:02:30 -0500

    fix

--- week4/config.js
+++ week4/config.js
@@ -4,5 +4,4 @@
 //
 //module.exports = config;
 
-//module.exports = "dongphilyoo:dwd2018@ds125628.mlab.com:25628/dwd-spring2018";
-exports = "dongphilyoo:dwd2018@ds125628.mlab.com:25628/dwd-spring2018";
+module.exports = "dongphilyoo:dwd2018@ds125628.mlab.com:25628/dwd-spring2018";
